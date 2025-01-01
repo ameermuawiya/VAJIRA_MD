24797542ff1acaaae8671ec0b3974e3cd36dc96f
ameermuawiya <81509990+ameermuawiya@users.noreply.github.com>
2025-01-01 05:14:36 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,14 +6,14 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Put your session id here' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=3AshiSoJ#0H9d1UU2UIRv3cAGXpsmkvX0GD4IhQGlPMoYKh0NdoI' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '+923423071137' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
-FOOTER: process.env.FOOTER=== undefined ? '‌👨‍💻 ᴠᴀᴊɪʀᴀ ᴍᴅ ʙʏ ᴛᴄ ᴛᴇᴀᴍ 👨‍💻': process.env.FOOTER,
+FOOTER: process.env.FOOTER=== undefined ? '‌𝙰𝚖𝚎𝚎𝚛 𝙼𝚞𝚊𝚠𝚒𝚢𝚊 𝚂𝚊𝚗𝚐𝚑𝚊': process.env.FOOTER,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
 MAX_SIZE: 500,
 PAIR: 'https://vajirasessionid-78fa4ff8a63a.herokuapp.com/code?number=',       
-ALIVE:  process.env.ALIVE  || '> VAJIRA MD'  ,    
+ALIVE:  process.env.ALIVE  || '> 𝚂𝚊𝚗𝚐𝚑𝚊 𝙱𝚘𝚝'  ,    
 DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO        
 };
